ee0c4b1bbd237b51d3d9cea75ccd7810d46570d4
iswyg <mr_nikit0s@live.ru>
2021-07-22 10:22:40 +0300

    left-menu

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
 		$('.header__burger-menu,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	$('.header__burger-catalog').click(function (event) {
+		$('.header__burger-catalog,.header__catalog').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 });
