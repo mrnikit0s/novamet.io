bca4b5205271f32aab9ec790cee6ac9d685eff33
iswyg <mr_nikit0s@live.ru>
2021-07-22 11:55:53 +0300

    catalog menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
+$menuFlag = -1;
+$catalogFlag = -1;
 $(document).ready(function () {
+
 	$('.header__burger-menu').click(function (event) {
-		$('.header__burger-menu,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
+		if ($catalogFlag == -1) {
+			$('.header__burger-menu,.header__menu').toggleClass('active');
+			$('body').toggleClass('lock');
+			$menuFlag = $menuFlag * -1;
+			$catalogFlag = -1;
+		}
 	});
 	$('.header__burger-catalog').click(function (event) {
-		$('.header__burger-catalog,.header__catalog').toggleClass('active');
-		$('body').toggleClass('lock');
+		if ($menuFlag == -1) {
+			$('.header__burger-catalog,.header__catalog').toggleClass('active');
+			$('body').toggleClass('lock');
+			$catalogFlag = $catalogFlag * (-1);
+			$menuFlag = -1;
+		}
 	});
 });
