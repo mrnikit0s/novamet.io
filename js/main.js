36a941877e0d569a565f0ff8339aed0af4aa519b
iswyg <mr_nikit0s@live.ru>
2021-07-31 22:19:27 +0300

    jquery media add

--- js/main.js
+++ js/main.js
@@ -11,6 +11,83 @@ $reloadFlag = 0;
 //------------------------ burger click--------------------------------------
 
 $(document).ready(function () {
+	/*********************** перестановка блоков ****************************/
+	if (window.matchMedia('(min-width: 320px)').matches) {
+		$('.header__search-btn').click(function () {
+			$('.header__search-block').toggleClass('active');
+			$('.header__search-form').toggleClass('active');
+		});
+	}
+	/*********************** перестановка блоков ****************************/
+	if (window.matchMedia('(min-width: 460px)').matches) {
+		$('.section1__content').before('<div class="section1__content-row"</div>');
+		$('.section1__content-row').append('<div class="section1__content-col"</div>');
+		$('.section1__content-row').append('<div class="section1__content-col"</div>');
+		/* левый блок */
+		$('.section1__logo').appendTo('.section1__content-col:first-child');
+		$('.section1__phone-black').appendTo('.section1__content-col:first-child');
+		$('.section1__whatsapp').appendTo('.section1__content-col:first-child');
+		/* правый блок */
+		$('.section1__phone-red').appendTo('.section1__content-col:last-child');
+		$('.section1__callback').appendTo('.section1__content-col:last-child');
+		$('.section1__price').appendTo('.section1__content-col:last-child');
+		$('.section1__calc').appendTo('.section1__content-col:last-child');
+		/* удаляем лишнее */
+		$('.section1__content').detach();
+	}
+	if (window.matchMedia('(min-width: 800px)').matches) {
+		for ($i = 0; $i < 2; $i++) {
+			$('.section1__content-row').append('<div class="section1__content-col"</div>');
+		}
+		//$('.section1__logo').appendTo('.section1__content-col:first-child');
+		/* второй столбец */
+		$('.section1__phone-red').appendTo('.section1__content-col:nth-child(2)');
+		$('.section1__callback').appendTo('.section1__content-col:nth-child(2)');
+		/* третий столбец */
+		$('.section1__phone-black').appendTo('.section1__content-col:nth-child(3)');
+		$('.section1__whatsapp').appendTo('.section1__content-col:nth-child(3)');
+		/* четвертый столбец */
+		$('.section1__price').appendTo('.section1__content-col:last-child');
+		$('.section1__calc').appendTo('.section1__content-col:last-child');
+	}
+	if (window.matchMedia('(min-width: 1024px)').matches) {
+		$('.topline').insertAfter('.section1');
+		$('.section1__content-row').append('<div class="section1__content-col"</div>');
+		/*---------------режим работы и email в 4 колонку --------------*/
+		$('.header__time').appendTo('.section1__content-col:nth-child(4)');
+		$('.header__email').appendTo('.section1__content-col:nth-child(4)');
+		/*---------------корзину в последнюю колонку -------------------*/
+		$('.topline__cart').appendTo('.section1__content-col:last-child');
+		$('.topline__cart__sum span').before('<div class="topline__cart-text">Сумма: </div>');
+
+		/*--------------- делим topline на колонки -------------------*/
+		$('.topline').append('<div class="topline__row"</div>');
+		for ($i = 0; $i < 3; $i++) {
+			$('.topline__row').append('<div class="topline__col"></div>');
+		}
+		/*---------------вставляем блоки--------------------*/
+		$('.topline__city').appendTo('.topline__col:first-child');
+		$('.header__search').appendTo('.topline__col:nth-child(2)');
+		$('.section1__price, .section1__calc').appendTo('.topline__col:last-child');
+		/*---------------переносим header------------------ */
+		$('.header').insertAfter('.topline');
+		/*---------------меняем местами колонки-------------*/
+		$('.section1__content-col:nth-child(4)').insertAfter('.section1__content-col:nth-child(2)');
+		/* header__catalog в content__aside */
+		$('.header__catalog').appendTo('.content__aside');
+		$('.header__catalog').addClass('content__catalog');
+	}
+	if (window.matchMedia('(min-width: 1200px)').matches) {
+		/*---------------добавляем контейнер topline----------------*/
+		$('<div class=container></div>').appendTo('.topline');
+		/*---------------переносим row в контейнер-----------*/
+		$('.topline__row').appendTo('.topline .container');
+		/*--------------------content__row в контейнер ------*/
+		$('.content__row').appendTo('.content>.container');
+	}
+	/*********************** перестановка блоков ****************************/
+
+
 	/* slider */
 	$('.slider').slick({
 		centerMode: true,
@@ -61,6 +138,7 @@ $(document).ready(function () {
 	});
 
 
+	//if (window.matchMedia('(mim-width: 320px)').matches) 
 
 
 	//------------------------------------------------- move element ------------------------
@@ -160,15 +238,13 @@ $(document).ready(function () {
 			$moveFlag1200 = 1;
 		}
 	}
-	/* 26.07
+
 	$(window).resize(function () {
-		$w = $(window).width();
+		location.reload();
+		//$w = $(window).width();
+		//moveNew();
 
 	});
-	*/
-	window.onresize = function () {
-		location.reload();
-	};
-	move($w);
 
+	//move($w);
 });
